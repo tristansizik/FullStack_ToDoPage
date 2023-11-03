844aef5828ad73b6e7c3bdfd7ab030562e7207a8
Tristan Sizik <tristansizikcode@gmail.com>
2023-11-03 13:49:09 -0700

    creating a 'useState' first before changing state

--- server/client/src/components/InputTodo.js
+++ server/client/src/components/InputTodo.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment , useState } from "react";
 
 const InputTodo = () => {
+
+    const [description, setDescription ] = useState("")
+
+
     return (
         <Fragment>
             <h1 className="text-center mt-5"> Pern Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={description} />
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
