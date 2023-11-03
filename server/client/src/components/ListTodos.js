7ed26adf783444715333a3a445f10ce8720d8d2d
Tristan Sizik <tristansizikcode@gmail.com>
2023-11-03 14:20:58 -0700

    updating listtodos, now shows the todolist in console from database!

--- server/client/src/components/ListTodos.js
+++ server/client/src/components/ListTodos.js
@@ -1,6 +1,25 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    const getTodos = async() => {
+        try {
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+
+            setTodos(jsonData);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    console.log(todos);
     return (
     <Fragment>
         <table class="table mt-5 text-center">
