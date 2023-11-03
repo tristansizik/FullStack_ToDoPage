f59886b95fa0316546514332e9dbf07633fc6b6f
Tristan Sizik <tristansizikcode@gmail.com>
2023-11-03 14:29:55 -0700

    updated listtodos to incorporate delete, also auto refresh show the website updating

--- server/client/src/components/ListTodos.js
+++ server/client/src/components/ListTodos.js
@@ -4,6 +4,19 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    //delete todo function
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+            
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos");
@@ -35,7 +48,16 @@ const ListTodos = () => {
                     <td>John</td>
                     <td>Doe</td>
                     <td>john@example.com</td>
-                </tr> */}                
+                </tr> */}      
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>Edit</td>
+                        <td>
+                            <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))}          
             </tbody>
         </table>
     </Fragment>
